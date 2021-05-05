4811c221a7a38f1bcf7d5c6cd25a647d8b4ac4d7
NeilDhote <neildhote@gmail.com>
2021-05-05 00:20:02 -0500

    Fixed Everything!

---
+++ app/src/main/java/com/example/VroomVroom/MainActivity.java
@@ -0,0 +1,277 @@
+package com.example.VroomVroom;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onClick(View view) {
+        if(btnRadioDriver.isChecked() == false && btnRadioPassenger.isChecked() == false) {
+            FancyToast.makeText(MainActivity.this,"Are you a driver or a passenger?",FancyToast.LENGTH_SHORT,FancyToast.INFO,false).show();
+            return;
+        }
+
+        if (ParseUser.getCurrentUser() == null) {
+
+            ParseAnonymousUtils.logIn(new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+
+                    if (user != null && e == null) {
+                        FancyToast.makeText(MainActivity.this,"We have an anonymous user",Toast.LENGTH_SHORT,FancyToast.INFO,false).show();
+
+                        if(btnRadioDriver.isChecked()) {
+                            user.put("as","Driver");
+
+                        } else if (btnRadioPassenger.isChecked()) {
+                            user.put("as","Passenger");
+                        }
+
+                        user.saveInBackground(new SaveCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                transitionToPassengerActivity();
+                                transitionToDriverRequestListActivity();
+
+                            }
+                        });
+
+                    }
+
+                }
+            });
+
+        }
+
+        /*
+        if(edtUserType.getText().toString().equals("Driver") || edtUserType.getText().toString().equals("Passenger")) {
+
+            if (ParseUser.getCurrentUser() == null) {
+
+                ParseAnonymousUtils.logIn(new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+
+                        if (user != null && e == null) {
+                            FancyToast.makeText(MainActivity.this,"We have an anonymous user",Toast.LENGTH_SHORT,FancyToast.INFO,false).show();
+
+                            user.put("as",edtUserType.getText().toString());
+
+                            user.saveInBackground(new SaveCallback() {
+                                @Override
+                                public void done(ParseException e) {
+                                    transitionToPassengerActivity();
+                                    transitionToDriverRequestListActivity();
+
+                                }
+                            });
+
+                        }
+
+                    }
+                });
+
+            }
+
+        } else {
+            FancyToast.makeText(MainActivity.this,"Are you a driver or a passenger?",FancyToast.LENGTH_SHORT,FancyToast.INFO,false).show();
+            return;
+
+        }
+        */
+
+    }
+
+    enum State {
+        SIGNUP,LOGIN;
+    }
+
+    public State state;
+
+    private EditText edtUsername, edtPassword, edtUserType;
+    private Button btnSignUp, btnOneTimeLogin;
+    private RadioButton btnRadioPassenger, btnRadioDriver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+        if (ParseUser.getCurrentUser() != null) {
+
+            //Transition
+            //ParseUser.logOut();
+            transitionToPassengerActivity();
+            transitionToDriverRequestListActivity();
+        }
+
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        btnRadioPassenger = findViewById(R.id.btnRadioPassenger);
+        btnRadioDriver = findViewById(R.id.btnRadioDriver);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnOneTimeLogin = findViewById(R.id.btnOneTimeLogin);
+
+        btnOneTimeLogin.setOnClickListener(this);
+
+        state = State.SIGNUP;
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(state == State.SIGNUP) {
+
+                    if(btnRadioDriver.isChecked() == false && btnRadioPassenger.isChecked() == false) {
+
+                        Toast.makeText(MainActivity.this,"Are you a driver or a passenger?",Toast.LENGTH_LONG).show();
+                        return;
+
+                    }
+
+                    ParseUser appUser = new ParseUser();
+                    appUser.setUsername(edtUsername.getText().toString());
+                    appUser.setPassword(edtPassword.getText().toString());
+                    if(btnRadioDriver.isChecked()) {
+                        appUser.put("as","Driver");
+
+                    } else if (btnRadioPassenger.isChecked()) {
+                        appUser.put("as","Passenger");
+
+                    }
+
+                    appUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+
+                            if (e == null) {
+
+                                FancyToast.makeText(MainActivity.this,"Signed Up!",Toast.LENGTH_SHORT,FancyToast.SUCCESS,true).show();
+
+                                transitionToPassengerActivity();
+                                transitionToDriverRequestListActivity();
+
+                            }
+
+                        }
+                    });
+
+                } else if (state == State.LOGIN) {
+
+
+                    ParseUser.logInInBackground(edtUsername.getText().toString(), edtPassword.getText().toString(), new LogInCallback() {
+                        @Override
+                        public void done(ParseUser user, ParseException e) {
+
+                            if(user != null && e == null) {
+
+                                FancyToast.makeText(MainActivity.this,"User Logged in!",Toast.LENGTH_SHORT,FancyToast.SUCCESS,true).show();
+
+                                transitionToPassengerActivity();
+                                transitionToDriverRequestListActivity();
+
+                            }
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.login_item:
+
+                if(state == State.SIGNUP) {
+
+                    state=State.LOGIN;
+                    item.setTitle("Sign Up");
+                    btnSignUp.setText("Log In");
+
+                } else if (state==State.LOGIN) {
+
+                    state=State.SIGNUP;
+                    item.setTitle("Log In");
+                    btnSignUp.setText("Sign Up");
+
+                }
+
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void transitionToPassengerActivity() {
+
+        if (ParseUser.getCurrentUser() != null) {
+
+            if (ParseUser.getCurrentUser().get("as").equals("Passenger")) {
+
+                Intent intent = new Intent(MainActivity.this,PassengerActivity.class);
+                startActivity(intent);
+
+            }
+
+        }
+
+    }
+
+    private void transitionToDriverRequestListActivity() {
+
+        if (ParseUser.getCurrentUser() != null) {
+
+            if (ParseUser.getCurrentUser().get("as").equals("Driver")) {
+
+                Intent intent = new Intent(this,DriverRequestListActivity.class);
+                startActivity(intent);
+
+            }
+
+        }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/VroomVroom/PassengerActivity.java
@@ -0,0 +1,446 @@
+package com.example.VroomVroom;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.DeleteCallback;
+import com.parse.FindCallback;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class PassengerActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
+
+    private GoogleMap mMap;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private Button btnRequestCar;
+    private Button btnBeep;
+
+    private boolean isVroomVroomCancelled = true;
+
+    private boolean isCarReady = false;
+    private Timer t;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        setTitle("Passenger");
+
+        btnRequestCar = findViewById(R.id.btnRequestCar);
+        btnRequestCar.setOnClickListener(this);
+
+        btnBeep = findViewById(R.id.btnBeep);
+        btnBeep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDriverUpdates();
+
+            }
+        });
+
+        ParseQuery<ParseObject> carRequestQuery = ParseQuery.getQuery("RequestCar");
+        carRequestQuery.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        carRequestQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+
+                if (objects.size() > 0 && e == null) {
+
+                    isVroomVroomCancelled = false;
+                    btnRequestCar.setText("Cancel your VroomVroom request!");
+
+                    getDriverUpdates();
+                }
+
+            }
+        });
+
+        findViewById(R.id.btnLogOutFromPassengerActivity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ParseUser.logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if (e == null) {
+
+                            finish();
+
+                            Intent intent = new Intent(PassengerActivity.this, MainActivity.class);
+                            startActivity(intent);
+
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+//    private Location getLastKnownLocation() {
+//        Location l=null;
+//        LocationManager mLocationManager = (LocationManager)getApplicationContext().getSystemService(LOCATION_SERVICE);
+//        List<String> providers = mLocationManager.getProviders(true);
+//        Location bestLocation = null;
+//        for (String provider : providers) {
+//            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED) {
+//                l = mLocationManager.getLastKnownLocation(provider);
+//            }
+//            if (l == null) {
+//                continue;
+//            }
+//            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+//                bestLocation = l;
+//            }
+//        }
+//        return bestLocation;
+//    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                updateCameraPassengerLocation(location);
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (Build.VERSION.SDK_INT < 23) {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        } else if (Build.VERSION.SDK_INT >= 23) {
+
+            if (ContextCompat.checkSelfPermission(PassengerActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(PassengerActivity.this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},1000);
+
+            } else {
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                Location currentPassengerLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                updateCameraPassengerLocation(currentPassengerLocation);
+
+            }
+
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1000 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            if (ContextCompat.checkSelfPermission(PassengerActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+                Location currentPassengerLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                updateCameraPassengerLocation(currentPassengerLocation);
+
+            }
+        }
+
+    }
+
+    private void updateCameraPassengerLocation(Location pLocation) {
+
+        if (isCarReady == false) {
+
+            try {
+                LatLng passengerLocation = new LatLng(pLocation.getLatitude(), pLocation.getLongitude());
+                mMap.clear(); //To remove other markers of location
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(passengerLocation)); //To move the map camera to user location
+                mMap.setMinZoomPreference(15f);
+                mMap.addMarker(new MarkerOptions().position(passengerLocation).title("You are here!!!").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        if (isVroomVroomCancelled) {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                Location passengerCurrentLocation = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+
+                if (passengerCurrentLocation != null) {
+
+                    ParseObject requestCar = new ParseObject("RequestCar");
+                    requestCar.put("username", ParseUser.getCurrentUser().getUsername());
+
+                    ParseGeoPoint userLocation = new ParseGeoPoint(passengerCurrentLocation.getLatitude(), passengerCurrentLocation.getLongitude());
+                    requestCar.put("passengerLocation", userLocation);
+
+                    requestCar.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+
+                            if (e == null) {
+
+                                Toast.makeText(PassengerActivity.this, "A car request is sent", Toast.LENGTH_SHORT).show();
+
+                                btnRequestCar.setText("Cancel your VroomVroom order");
+                                isVroomVroomCancelled = false;
+
+                            }
+
+                        }
+                    });
+
+                } else {
+
+                    Toast.makeText(this, "Unknown Error. Something went wrong!!!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        } else {
+
+            ParseQuery<ParseObject> carRequestQuery = ParseQuery.getQuery("RequestCar");
+            carRequestQuery.whereEqualTo("username",ParseUser.getCurrentUser().getUsername());
+            carRequestQuery.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> requestList, ParseException e) {
+
+                    if (requestList.size() > 0 && e == null) {
+
+                        isVroomVroomCancelled = true;
+                        btnRequestCar.setText("Request a new VroomVroom");
+
+                        for (ParseObject VroomVroomRequest : requestList ) {
+
+                            VroomVroomRequest.deleteInBackground(new DeleteCallback() {
+                                @Override
+                                public void done(ParseException e) {
+
+                                    if (e == null) {
+
+                                        Toast.makeText(PassengerActivity.this,"Requests deleted",Toast.LENGTH_SHORT).show();
+
+                                    }
+
+                                }
+                            });
+
+                        }
+
+                    }
+
+                }
+            });
+
+        }
+
+    }
+
+    private void getDriverUpdates() {
+
+        t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+
+                ParseQuery<ParseObject> VroomVroomRequestQuery = ParseQuery.getQuery("RequestCar");
+                VroomVroomRequestQuery.whereEqualTo("username",ParseUser.getCurrentUser().getUsername());
+                VroomVroomRequestQuery.whereEqualTo("requestAccepted",true);
+                VroomVroomRequestQuery.whereExists("driverOfMe");
+
+                VroomVroomRequestQuery.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(final List<ParseObject> objects, ParseException e) {
+
+                        if (objects.size() > 0 && e == null) {
+
+                            isCarReady = true;
+                            for (final ParseObject requestObject : objects) {
+
+                                ParseQuery<ParseUser> driverQuery = ParseUser.getQuery();
+                                driverQuery.whereEqualTo("username",requestObject.getString("driverOfMe"));
+                                driverQuery.findInBackground(new FindCallback<ParseUser>() {
+                                    @Override
+                                    public void done(List<ParseUser> drivers, ParseException e) {
+                                        if (drivers.size() > 0 && e == null) {
+
+                                            for (ParseUser driverOfRequest : drivers) {
+
+                                                ParseGeoPoint driverOfRequestLocation = driverOfRequest.getParseGeoPoint("driverLocation");
+                                                if (ContextCompat.checkSelfPermission(PassengerActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                                                    Location passengerLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                                                    ParseGeoPoint pLocationAsParseGeoPoint = new ParseGeoPoint(passengerLocation.getLatitude(),passengerLocation.getLongitude());
+
+                                                    double milesDistance = driverOfRequestLocation.distanceInMilesTo(pLocationAsParseGeoPoint);
+
+                                                    if(milesDistance < 0.3) {
+
+                                                        requestObject.deleteInBackground(new DeleteCallback() {
+                                                            @Override
+                                                            public void done(ParseException e) {
+                                                                if ( e == null ) {
+
+                                                                    Toast.makeText(PassengerActivity.this, "Your VroomVroom is ready! Hurray", Toast.LENGTH_LONG).show();
+                                                                    isCarReady = false;
+                                                                    isVroomVroomCancelled = true;
+                                                                    btnRequestCar.setText("You can order a new VroomVroom now!");
+
+                                                                }
+                                                            }
+                                                        });
+
+                                                    } else {
+
+                                                        float roundedDistance = Math.round(milesDistance * 10) / 10;
+                                                        Toast.makeText(PassengerActivity.this, requestObject.getString("driverOfMe") + " is " + roundedDistance + " miles away from you!- Please wait!!!", Toast.LENGTH_LONG).show();
+
+                                                        LatLng dLocation = new LatLng(driverOfRequestLocation.getLatitude(),
+                                                                driverOfRequestLocation.getLongitude());
+
+                                                        LatLng pLocation = new LatLng(pLocationAsParseGeoPoint.getLatitude(), pLocationAsParseGeoPoint.getLongitude());
+
+                                                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                                                        Marker driverMarker = mMap.addMarker(new MarkerOptions().position(dLocation).title("Driver Location"));
+                                                        Marker passengerMarker = mMap.addMarker(new MarkerOptions().position(pLocation).title("Passenger Location"));
+
+                                                        ArrayList<Marker> myMarkers = new ArrayList<>();
+                                                        myMarkers.add(driverMarker);
+                                                        myMarkers.add(passengerMarker);
+
+                                                        for (Marker marker : myMarkers) {
+
+                                                            builder.include(marker.getPosition());
+
+                                                        }
+
+                                                        LatLngBounds bounds = builder.build();
+
+                                                        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 80);
+                                                        mMap.animateCamera(cameraUpdate);
+                                                    }
+
+                                                }
+
+                                            }
+
+                                        }
+                                    }
+                                });
+
+
+                            }
+
+                        } else {
+
+                            isCarReady = false;
+
+                        }
+
+                    }
+                });
+
+            }
+
+        }, 0, 3000);
+
+
+    }
+
+}
